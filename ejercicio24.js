f64d38a2411a9b6edc37add6b32f67e06f747b1d
Roberto Flores <robertoflores@MacBook-Air-de-Roberto.local>
2023-09-27 21:41:44 -0500

    Laboratorio Terminado

---
+++ ejercicio24.js
@@ -0,0 +1,6 @@
+miFuncion(2,4);
+
+function miFuncion(a,b){
+    console.log("Suma: " + (a + b));
+}
+miFuncion(2,3);
---
+++ ejercicio6.js
@@ -0,0 +1,13 @@
+let nombreCompleto = "Juan Perez";
+let nombrecompleto = "Carlos Perez";
+
+console.log(nombreCompleto);
+console.log(nombrecompleto);
+
+let alnombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+//let 1nombreCompleto; no esta permitido iniciar
+// el nombre de una variable con numeros
+
+let ruptura = 10;
